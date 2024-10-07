b684fa9fcea0be9a82f0733f2be1783a600e37cb
Nishchay Sinha <61420072+nishchaysinha@users.noreply.github.com>
2024-10-07 11:10:58 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request
+from flask_cors import CORS  # Import CORS
 from vision import get_predictions
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 @app.route('/', methods=['GET', 'POST'])
 def index(): 
@@ -13,7 +15,6 @@ def index():
             predictions = get_predictions(file_image)
             return render_template("index.html", predictions=predictions)
     return render_template('index.html', predictions=None)
-
 
 if __name__ == "__main__":
     app.run(debug=True)

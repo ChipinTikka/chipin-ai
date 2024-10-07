0a5513b9b697f46c42831809184095ea93c36289
Nishchay Sinha <61420072+nishchaysinha@users.noreply.github.com>
2024-10-07 11:08:39 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+from vision import get_predictions
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index(): 
+    if request.method == 'GET':
+        return render_template('index.html', predictions=None)
+    elif request.method == 'POST' and request.files:
+        file_image = request.files['image']
+        if file_image:
+            predictions = get_predictions(file_image)
+            return render_template("index.html", predictions=predictions)
+    return render_template('index.html', predictions=None)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
